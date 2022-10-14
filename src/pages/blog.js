a245ae8824b5afdabd31a373e6a325eded6367f5
Nicolas Sammaritano <sammaritanonicolas@gmail.com>
2022-10-14 11:17:16 -0700

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,24 +5,30 @@ import { graphql } from 'gatsby'
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                    {node.name}
-                    </li>
-                ))}
-            </ul>
+             {
+                data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                </article>
+                ))
+            }
         </Layout>
     )
 }
 export const query = graphql`
-    query{
-        allFile {
-            nodes {
-                name
-            }
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
+  }
 `
 export const Head = () => <Seo title="My Blog Posts" />
 
